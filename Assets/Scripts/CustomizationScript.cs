a8fe0bad183b18e8e40666ff8f09b7c64d0ea8d5
ReDerEE <108543563+ReDerEE@users.noreply.github.com>
2025-06-13 11:23:25 +0300

    Lisatud tegelase värvi muutmine customization menüüs ja see saadab värvi edasi mängule

--- Assets/Scripts/CustomizationScript.cs
+++ Assets/Scripts/CustomizationScript.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomizationScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public ColorChanger colorChanger;
+    public Slider HeadRed, HeadGreen, HeadBlue;
+    public Slider BodyRed, BodyGreen, BodyBlue;
+
+    public void changeHeadRed()
     {
-        
+        colorChanger.headRed = HeadRed.value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void changeHeadGreen()
     {
-        
+        colorChanger.headGreen = HeadGreen.value;
+    }
+
+    public void changeHeadBlue()
+    {
+        colorChanger.headBlue = HeadBlue.value;
+    }
+
+    public void changeBodyRed()
+    {
+        colorChanger.bodyRed = BodyRed.value;
+    }
+
+    public void changeBodyGreen()
+    {
+        colorChanger.bodyGreen = BodyGreen.value;
+    }
+
+    public void changeBodyBlue()
+    {
+        colorChanger.bodyBlue = BodyBlue.value;
     }
 }
